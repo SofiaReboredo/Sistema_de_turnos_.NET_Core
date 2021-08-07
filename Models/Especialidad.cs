aea1b0f5a08d8a13dc34c32f67ef6725312faa01
SofiaReboredo <sofi.reboredo@gmail.com>
2021-08-07 18:56:47 -0300

    Updated

---
+++ Models/Especialidad.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Turnos.Models
+{
+    public class Especialidad
+    {
+        [Key]
+        public int  IdEspecialidad { get; set; }
+        public string Descripcion { get; set; }
+        public List<MedicoEspecialidad> MedicoEspecialidad {get; set;}
+    }
+}
