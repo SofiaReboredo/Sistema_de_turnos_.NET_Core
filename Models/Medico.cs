3459e607dbde1a43ff278160e41a1b683f124a13
SofiaReboredo <sofi.reboredo@gmail.com>
2021-08-16 16:21:49 -0300

    Agrega Data Annotations

--- Models/Medico.cs
+++ Models/Medico.cs
@@ -7,13 +7,29 @@ namespace Turnos.Models
     public class Medico
     {
         [Key]
-        public int IdMedico {get; set;}
-        public string Nombre {get; set;}
-        public string Apellido {get; set;}
-        public string Direccion {get; set;}
-        public string Email {get; set;}
-        public DateTime HorarioAtencionDesde {get; set;}
-        public DateTime HorarioAtencionHasta {get; set;}
-        public List<MedicoEspecialidad> MedicoEspecialidad {get; set;}
+        public int IdMedico { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un nombre")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un apellido")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "Debe ingresar una dirección")]
+        [Display(Name = "Dirección")]
+        public string Direccion { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un email")]
+        [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "No es una dirección de email válida")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un horario de atención desde el cual atenderá")]
+        [Display(Name = "Horario desde")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
+        public DateTime HorarioAtencionDesde { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un horario de atención hasta el cual atenderá")]
+        [Display(Name = "Horario hasta")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
+        public DateTime HorarioAtencionHasta { get; set; }
+        [Required(ErrorMessage = "Debe ingresar una especialidad")]
+        public List<MedicoEspecialidad> MedicoEspecialidad { get; set; }
     }
 }
